78fd4a4075c70a9e018468f16a13ff876c449a02
znlehandwyj <zhuge1973vyh@126.com>
2021-03-21 22:34:53 +0800

    Eureka注册的服务，被客户端 8001客户端发现，EnableDiscoveryClient

--- mircoservicecloud/microservicecloud-provide-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
+++ mircoservicecloud/microservicecloud-provide-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
@@ -2,12 +2,18 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaClient//本来服务启动后会自动注册进eureka服务中
+@EnableDiscoveryClient
 public class DeptProvider8001_App {
 	public static void main(String[] args) {
+		
+//		String toUp = "microservice-dept8001".toUpperCase();
+//		System.out.println(toUp);
+		
 		SpringApplication.run(DeptProvider8001_App.class, args);
 	}
 }
