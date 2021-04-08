b4ef053ed8e31306fb8e1d3759570e0a228faa63
znlehandwyj <zhuge1973vyh@126.com>
2021-04-09 00:53:11 +0800

    Ribbon配置初步，简单的启动 3个Eureka服务之后，再启动1个8001的服务提供端，然后再启动80的客户端。记得80要配置yml，否则会报错

--- mircoservicecloud/microservice-consumer-80/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ mircoservicecloud/microservice-consumer-80/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -13,8 +13,14 @@ import com.atguigu.springcloud.common.bean.Dept;
 @RestController
 public class DeptController {
 
-	private static final String REST_URL_PREFIX = "http://localhost:8001";
+//	private static final String REST_URL_PREFIX = "http://localhost:8001";
+	private static final String REST_URL_PREFIX = "http://MICROSERVICECLOUD-DEPT";
 	
+	
+	/**
+	 * 使用 使用restTemplate访问restful接口非常的简单粗暴无脑。 (url, requestMap,
+	 * ResponseBean.class)这三个参数分别代表 REST请求地址、请求参数、HTTP响应转换被转换成的对象类型。
+	 */
 	@Autowired
 	private RestTemplate restTemplate;
 	
@@ -23,4 +29,8 @@ public class DeptController {
 	public List<Dept> list(){
 		return restTemplate.getForObject( REST_URL_PREFIX +"/dept/list", List.class);
 	}
+	
+//	public static void main(String[] args) {
+//		System.out.println("http://microservicecloud-dept".toUpperCase());
+//	}
 }
